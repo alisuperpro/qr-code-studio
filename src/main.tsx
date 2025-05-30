6910c58c7d1c426473a195ebf824b69289dbc313
alisuperpro <alisolorzanove@gmail.com>
2025-05-30 18:49:14 -0400

    change import

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Toaster } from '@/components/ui/toaster'
-import './index.css'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-    <Toaster />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+        <Toaster />
+    </React.StrictMode>
 )
