a69b4f264d2f7f12713a0c9e5d95ca8a38628c6b
alisuperpro <alisolorzanove@gmail.com>
2025-05-30 17:19:36 -0400

    fix bug

--- src/components/QrText.tsx
+++ src/components/QrText.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 export const QrText = () => {
     const [text, setText] = useState('')
     const setContent = useQrStore((state) => state.setContent)
-    const content = useQrStore((state) => state.content)
 
     const handlerText = (value: string) => {
         setContent(value)
@@ -16,7 +15,7 @@ export const QrText = () => {
             <textarea
                 className="textarea-sizing px-2 py-2  outline-1 border-[1px] border-black text-lg max-h-96 h-96 w-full resize-none rounded-sm shadow-md"
                 placeholder="Escribe el texto"
-                value={content}
+                value={text}
                 onChange={(event) => handlerText(event.target.value)}
             ></textarea>
         </section>
