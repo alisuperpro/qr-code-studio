776ea49cfe7175dcd483e549573c4e0e9fb590e6
alisuperpro <alisolorzanove@gmail.com>
2025-05-30 17:14:24 -0400

    feat: fix bugs

--- src/components/QrText.tsx
+++ src/components/QrText.tsx
@@ -1,17 +1,24 @@
 import { useQrStore } from '@/store/qrStore'
+import { useState } from 'react'
 
 export const QrText = () => {
-  const setContent = useQrStore((state) => state.setContent)
-  const content = useQrStore((state) => state.content)
+    const [text, setText] = useState('')
+    const setContent = useQrStore((state) => state.setContent)
+    const content = useQrStore((state) => state.content)
 
-  return (
-    <section className="px-2 pt-6">
-      <textarea
-        className="textarea-sizing px-2 py-2  outline-1 border-[1px] border-black text-lg max-h-96 h-96 w-full resize-none rounded-sm shadow-md"
-        placeholder="Escribe el texto"
-        value={content}
-        onChange={(event) => setContent(event.target.value)}
-      ></textarea>
-    </section>
-  )
+    const handlerText = (value: string) => {
+        setContent(value)
+        setText(value)
+    }
+
+    return (
+        <section className="px-2 pt-6">
+            <textarea
+                className="textarea-sizing px-2 py-2  outline-1 border-[1px] border-black text-lg max-h-96 h-96 w-full resize-none rounded-sm shadow-md"
+                placeholder="Escribe el texto"
+                value={content}
+                onChange={(event) => handlerText(event.target.value)}
+            ></textarea>
+        </section>
+    )
 }
--- src/components/QrUrl.tsx
+++ src/components/QrUrl.tsx
@@ -3,40 +3,49 @@ import { ClipboardPaste } from 'lucide-react'
 import { Label } from './ui/label'
 import { useClipboard } from '@/hooks/useClipboard'
 import { useQrStore } from '@/store/qrStore'
+import { useState } from 'react'
 
 export const QrUrl = () => {
-  const content = useQrStore((state) => state.content)
-  const setContent = useQrStore((state) => state.setContent)
-  const { pasteText } = useClipboard()
+    const [url, setUrl] = useState('')
+    const setContent = useQrStore((state) => state.setContent)
+    const { pasteText } = useClipboard()
 
-  const pasteLink = async (event: any) => {
-    event.preventDefault()
-    const text = await pasteText()
-    setContent(text)
-  }
-  return (
-    <section className="w-full px-4 py-2 flex justify-center items-center h-full">
-      <div className="w-full">
-        <div className=" flex justify-center items-center">
-          <div className="flex justify-center items-center flex-col gap-y-2 ">
-            <div className="">
-              <Label htmlFor="url">URL</Label>
-              <div className="w-full flex flex-row justify-center items-center gap-x-2">
-                <textarea
-                  id="url"
-                  value={content}
-                  onChange={(event) => setContent(event.target.value)}
-                  placeholder="https://cachemarketing.net/"
-                  className="textarea-sizing w-60 max-w-60 max-h-40 resize-none px-2 py-2 outline-1 border-[1px] rounded-sm border-black"
-                ></textarea>
-              </div>
-              <Button onClick={pasteLink} className="my-2">
-                <ClipboardPaste /> Pegar
-              </Button>
+    const pasteLink = async (event: any) => {
+        event.preventDefault()
+        const text = await pasteText()
+        setContent(text)
+    }
+
+    const handlerUrl = (value: string) => {
+        setUrl(value)
+        setContent(value)
+    }
+
+    return (
+        <section className="w-full px-4 py-2 flex justify-center items-center h-full">
+            <div className="w-full">
+                <div className=" flex justify-center items-center">
+                    <div className="flex justify-center items-center flex-col gap-y-2 ">
+                        <div className="">
+                            <Label htmlFor="url">URL</Label>
+                            <div className="w-full flex flex-row justify-center items-center gap-x-2">
+                                <textarea
+                                    id="url"
+                                    value={url}
+                                    onChange={(event) =>
+                                        handlerUrl(event.target.value)
+                                    }
+                                    placeholder="https://cachemarketing.net/"
+                                    className="textarea-sizing w-60 max-w-60 max-h-40 resize-none px-2 py-2 outline-1 border-[1px] rounded-sm border-black"
+                                ></textarea>
+                            </div>
+                            <Button onClick={pasteLink} className="my-2">
+                                <ClipboardPaste /> Pegar
+                            </Button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
