3dca16445929ed0863d9f49031d90a13cf65d5e6
alisuperpro <alisolorzanove@gmail.com>
2025-05-30 18:52:08 -0400

    Update textarea style

--- src/components/QrEmail.tsx
+++ src/components/QrEmail.tsx
@@ -5,61 +5,61 @@ import { useQrStore } from '@/store/qrStore'
 import { textToUrl } from '@/utils/functions'
 
 export const QrEmail = () => {
-  const [email, setEmail] = useState('')
-  const [subject, setSubject] = useState('')
-  const [body, setBody] = useState('')
-  const setContent = useQrStore((state) => state.setContent)
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+    const [body, setBody] = useState('')
+    const setContent = useQrStore((state) => state.setContent)
 
-  useEffect(() => {
-    const convert = `mailto:${email}?subject=${subject}&body=${body}`
+    useEffect(() => {
+        const convert = `mailto:${email}?subject=${subject}&body=${body}`
 
-    if (email.length >= 1) {
-      setContent(convert)
+        if (email.length >= 1) {
+            setContent(convert)
+        }
+    }, [email, subject, body])
+
+    const handleSubjectInput = (subject: string) => {
+        const subjectUrl = textToUrl(subject)
+        setSubject(subjectUrl)
     }
-  }, [email, subject, body])
 
-  const handleSubjectInput = (subject: string) => {
-    const subjectUrl = textToUrl(subject)
-    setSubject(subjectUrl)
-  }
+    const handleBodyInput = (body: string) => {
+        const bodyUrl = textToUrl(body)
+        setBody(bodyUrl)
+    }
+    return (
+        <section className="flex flex-col justify-center items-center w-full gap-y-2 px-6 py-2 pt-16">
+            <div className="w-full flex flex-row justify-center items-center gap-x-2 ">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    className=" outline-1 border rounded-sm border-black ml-3"
+                />
+            </div>
 
-  const handleBodyInput = (body: string) => {
-    const bodyUrl = textToUrl(body)
-    setBody(bodyUrl)
-  }
-  return (
-    <section className="flex flex-col justify-center items-center w-full gap-y-2 px-6 py-2 pt-16">
-      <div className="w-full flex flex-row justify-center items-center gap-x-2 ">
-        <Label htmlFor="email">Email</Label>
-        <Input
-          type="email"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          className=" outline-1 border rounded-sm border-black ml-3"
-        />
-      </div>
-
-      <div className="w-full  flex flex-row justify-center items-center gap-x-2">
-        <Label htmlFor="subject">Asunto</Label>
-        <Input
-          type="text"
-          name="subject"
-          id="subject"
-          className=" outline-1 border rounded-sm border-black"
-          value={subject}
-          onChange={(event) => handleSubjectInput(event.target.value)}
-        />
-      </div>
-      <div className="w-full">
-        <textarea
-          name="body"
-          className="w-full max-h-60 h-60 resize-none textarea-sizing shadow-md px-2 py-2  outline-1 border rounded-sm border-black"
-          value={body}
-          onChange={(event) => handleBodyInput(event.target.value)}
-        ></textarea>
-      </div>
-    </section>
-  )
+            <div className="w-full  flex flex-row justify-center items-center gap-x-2">
+                <Label htmlFor="subject">Asunto</Label>
+                <Input
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    className=" outline-1 border rounded-sm border-black"
+                    value={subject}
+                    onChange={(event) => handleSubjectInput(event.target.value)}
+                />
+            </div>
+            <div className="w-full">
+                <textarea
+                    name="body"
+                    className="w-full max-h-60 h-60 resize-none textarea-sizing shadow-md px-2 py-2 border rounded-sm border-black"
+                    value={body}
+                    onChange={(event) => handleBodyInput(event.target.value)}
+                ></textarea>
+            </div>
+        </section>
+    )
 }
